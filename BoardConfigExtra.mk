50f17bf9cc8551e8fb1e82e440c7435a1195226f
razorloves <razorloves@gmail.com>
2018-02-01 23:27:58 -0600

    marlin: add CarrierConfig app vendor overlay
    This xml was extracted from the GoogleCarrierConfig app in the marlin
    8.0 factory image. It cannot be extracted from the 8.1 factory image
    because google stopped using the GoogleCarrierConfig app for adding
    vendor carrier configurations. They now use a new app named
    CarrierSettings, but it's not in a format compatible with the aosp
    CarrierConfig app.
    
    Change-Id: I2177c874693669f549554d53675e723159b0f071
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,7 +1,10 @@
 # Custom BoardConfig for marlin
 
 # Inline kernel building
-TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
+TARGET_KERNEL_CONFIG := king_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CLANG_COMPILE := true
+# Version will be changed to 9.0
+TARGET_KERNEL_CLANG_VERSION := 6.0.2
