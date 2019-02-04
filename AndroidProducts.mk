ec08a71fac6657517617ea33f9b46a6eda6029c9
kingbri <bdashore3@gmail.com>
2019-02-04 18:03:51 -0500

    Add havoc_marlin to products

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,5 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
 	$(LOCAL_DIR)/aosp_marlin_svelte.mk \
-	$(LOCAL_DIR)/aosp_sailfish.mk
-<<<<<<< HEAD
-=======
-
->>>>>>> fec49d25... tree bringup from source
+	$(LOCAL_DIR)/aosp_sailfish.mk \
+	$(LOCAL_DIR)/havoc_marlin.mk
