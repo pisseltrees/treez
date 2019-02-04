06ad36c01ed5a70fddd92e65f5e42ae7eba289e5
kingbri <bdashore3@gmail.com>
2019-02-04 18:03:14 -0500

    Add device-extra.mk

---
+++ device-extra.mk
@@ -0,0 +1,28 @@
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
+
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager.xml \
+    com.android.ims.rcsmanager \
+    RcsService \
+    PresencePolling
+
+# Tethering
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
+# Update engine
+PRODUCT_PACKAGES += brillo_update_payload
+
+# Nexus.xml package file
+PRODUCT_COPY_FILES += \
+    device/google/marlin/nexus.xml:system/etc/sysconfig/nexus.xml
+
+# Google Assistant
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.opa.eligible_device=true
+
+#Permissons
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.control_privapp_permissions=log
